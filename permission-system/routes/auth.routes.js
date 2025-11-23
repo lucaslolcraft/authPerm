27683ec9212808e3fd9a713123c154e94206a8e3
lucaslolcraft <lucaslolwer@gmail.com>
2025-11-23 12:02:46 -0300

    refazer o swagger e fazer um teste de permissoes

--- permission-system/routes/auth.routes.js
+++ permission-system/routes/auth.routes.js
@@ -28,4 +28,35 @@ router.get('/permissoes/:usuario_id', (req, res) => {
   });
 });
 
+router.get('/protegido/relatorio-financeiro/:usuario_id', (req, res) => {
+  const { usuario_id } = req.params;
+
+  const query = `
+  SELECT p.nome
+  FROM usuario u
+  JOIN usuario_grupo ug ON u.id = ug.usuario_id
+  JOIN grupo g ON ug.grupo_id = g.id
+  JOIN grupo_permissao gp ON g.id = gp.grupo_id
+  JOIN permissao p ON gp.permissao_id = p.id
+  WHERE u.id = ? AND p.nome = 'VER_RELATORIO'
+  `;
+
+  db.get(query, [usuario_id], (err, row) => {
+    if (err) {
+      return res.status(500).json({ erro: err.message });
+    }
+
+    if (!row) {
+      return res.status(403).json({
+        mensagem: "Acesso negado: você não tem a permissão VER_RELATORIO"
+      });
+    }
+
+    res.json({
+      mensagem: "Acesso liberado! Aqui está o relatório financeiro"
+    });
+  });
+});
+
+
 module.exports = router;
